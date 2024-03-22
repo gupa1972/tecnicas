b602ad821092ee97005f2c3514228849a707b5c8
gupa1972 <163356605+gupa1972@users.noreply.github.com>
2024-03-21 21:36:10 -0300

    Clase jueves

---
+++ tecnicas/python/promedio.py
@@ -0,0 +1,8 @@
+nota1=int(input("Ingresa Primera Nota "))
+nota2=int(input("Ingresa Segunda Nota "))
+nota3=int(input("Ingresa Tercera Nota "))
+resProm=((nota1+nota2+nota3)/3)
+print(resProm),
+
+
+
